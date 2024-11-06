5c1ecdf4f4890f9b6a5940c536211cbb1802bf70
ryanyeap <ryanyeap@hotmail.com>
2024-11-06 12:43:05 +0800

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-black font-black">
+    <h1 className="text-3xl font-black text-black">
       Welcome to the Together Trip App
     </h1>
   );
